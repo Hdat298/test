c2e7e67503123fa434416493937b2fa9e4892f11
Hoang Dat <hdat318@gmail.com>
2022-11-25 23:08:08 +0700

    Add files via upload

---
+++ func.js
@@ -0,0 +1,36 @@
+const clip1 = document.querySelector('.clip1');
+const clip2 = document.querySelector('.clip2');
+const clip3 = document.querySelector('.clip3');
+const clip4 = document.querySelector('.clip4');
+function play1() {
+    document.querySelector('.clip1');
+    clip1.play();
+}
+function play11() {
+    document.querySelector('.clip1');
+    clip1.pause();
+}
+function play2() {
+    document.querySelector('.clip2');
+    clip2.play();
+}
+function play22() {
+    document.querySelector('.clip2');
+    clip2.pause();
+}
+function play3() {
+    document.querySelector('.clip3');
+    clip3.play();
+}
+function play33() {
+    document.querySelector('.clip3');
+    clip3.pause();
+}
+function play4() {
+    document.querySelector('.clip4');
+    clip4.play();
+}
+function play44() {
+    document.querySelector('.clip4');
+    clip4.pause();
+}
